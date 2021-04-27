6a9b2f332f7ad1fdef96873d68b39deebbd62610
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-04-27 15:36:45 -0300

    feat: add ssg to price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { GetServerSideProps, GetServerSidePropsResult } from "next";
+import { GetStaticProps } from "next";
 import { ContentContainer, Hero } from "../styles/home";
 import SubscribeButton from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IkuZXBggNW78IlrhUAbIKZV", {
     expand: ["product"],
   });
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
