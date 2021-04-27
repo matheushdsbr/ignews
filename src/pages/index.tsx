9a2870d8ed84bee2d3775aa60bb71d4cc786cdb8
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-04-27 13:53:51 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from "next/head";
+import { ContentContainer, Hero } from "../styles/home";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <ContentContainer>
+        <Hero>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </Hero>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </ContentContainer>
     </>
   );
 }
